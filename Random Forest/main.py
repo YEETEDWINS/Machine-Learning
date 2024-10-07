b9e4e2beeea35a9e4c8fb5e053a80fc96ec58a9b
YEETEDWINS <164928968+YEETEDWINS@users.noreply.github.com>
2024-10-07 18:23:02 +0200

    Add files via upload

--- Random Forest/main.py
+++ Random Forest/main.py
@@ -2,6 +2,9 @@ import pandas as pd
 import matplotlib as mpl
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, classification_report
+import os
 
 data = pd.read_csv("/Users/edwinkcijo/Desktop/just for india/Machine Learning/Random Forest/adult_income.csv", sep=", ")
 
@@ -20,4 +23,15 @@ print(y[:5])
 print(data.info())
 
 
-xtrain, xtest, ytrain, ytest = train_test_split(X, y, test_size=0.2, random_state=0.5)
+xtrain, xtest, ytrain, ytest = train_test_split(X, y, test_size=0.2, random_state=5)
+
+Classifier = RandomForestClassifier(n_estimators=100) # default is 100, it is the number of decision trees used
+Classifier.fit(xtrain, ytrain)
+
+predicted = Classifier.predict(xtest)
+print(predicted)
+
+os.system("clear")
+matrix = confusion_matrix(ytest, predicted)
+print(classification_report(ytest, predicted))
+print(matrix)
