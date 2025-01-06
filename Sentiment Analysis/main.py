f646f74bb010016fc5aa5ba4b060830e2b3b9163
YEETEDWINS <164928968+YEETEDWINS@users.noreply.github.com>
2025-01-06 18:59:58 +0100

    Add files via upload

--- Sentiment Analysis/main.py
+++ Sentiment Analysis/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, classification_report
 import nltk
 import re
 
@@ -8,11 +10,20 @@ print(data["emotion"].value_counts())
 data["emotion"].replace({"joy": 1, "love": 1, "surprise": 1, "sadness": 0, "anger": 0, "fear": 0}, inplace=True)
 print(data["emotion"].value_counts())
 
-x_train, y_train, x_test, y_test = train_test_split(data["text"], data["emotion"], train_size=0.7, random_state=50)
+x_train, x_test, y_train, y_test = train_test_split(data["text"], data["emotion"], train_size=0.7, random_state=50)
 
-# nltk.download('stopwords')
-# nltk.download('wordnet')
-# from nltk.corpus import stopwords
+import ssl
+
+try:
+  _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+  pass
+else:
+  ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 objLemmatizer = WordNetLemmatizer()
 
@@ -25,12 +36,39 @@ def textTransform(texts):
     lemma = []
 
     for word in normal:
-      # if word not in set(stopwords.words('english')):
-      if word not in nltk.corpus.stopwords
+      if word not in set(stopwords.words('english')):
+      # if word not in nltk.corpus.stopwords:
         lemma.append(objLemmatizer.lemmatize(word))
     corpus.append(' '.join(lemma))
   return corpus
 
 print('corpus')
-processedX = textTransform(x_train)
-print(processedX[:9])
+processedtrain = textTransform(x_train)
+print(processedtrain[:9])
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(ngram_range=(1,2))
+x_train = cv.fit_transform(processedtrain)
+rfc = RandomForestClassifier()
+rfc.fit(x_train, y_train)
+
+processedtest = cv.transform(textTransform(x_test))
+y_pred = rfc.predict(processedtest)
+print(y_pred)
+
+matrix = confusion_matrix(y_test, y_pred)
+print(matrix)
+print(classification_report(y_test, y_pred))
+
+def identify(statement):
+  lemmatext = textTransform(statement)
+  datatext = cv.transform(lemmatext)
+  prediction = rfc.predict(datatext)
+  # for value in range(len(prediction)):
+  #   if prediction[value] == 1:
+  #     prediction[value] = "+"
+  #   elif prediction[value] == 0:
+  #     prediction[value] = "-"
+  print(prediction)
+
+identify(["I love cheeseburgers","School... It is a terrible place, a nightmare to say the least","Winning is a lot of fun","To kill two birds with one stone makes tasks much easier", "I like eating peanut protein bars"])
